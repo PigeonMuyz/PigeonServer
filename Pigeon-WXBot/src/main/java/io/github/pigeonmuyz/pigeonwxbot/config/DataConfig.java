319fe13f0ae7d2e3da826d3957e55f6d0bf219f8
PigeonMuyz <muyz0319@icloud.com>
2024-10-15 09:07:02 +0800

    增加定期向数据库拉绑定的用户数据

--- Pigeon-WXBot/src/main/java/io/github/pigeonmuyz/pigeonwxbot/config/DataConfig.java
+++ Pigeon-WXBot/src/main/java/io/github/pigeonmuyz/pigeonwxbot/config/DataConfig.java
@@ -1,18 +1,24 @@
 package io.github.pigeonmuyz.pigeonwxbot.config;
 
+import io.github.pigeonmuyz.pigeonwxbot.entity.ChannelBind;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Component
+@EnableScheduling
 public class DataConfig {
     private static final Logger logger = LoggerFactory.getLogger(DataConfig.class);
     @Value("${user.api-config-id}")
@@ -21,6 +27,10 @@ public class DataConfig {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    public static List<ChannelBind> channelBinds = new ArrayList<>();
+
+    public static String botAddress = "";
 
     @PostConstruct
     @Scheduled(fixedDelay = 7200000) //每2小时执行一次
@@ -35,5 +45,14 @@ public class DataConfig {
             logger.error("JXAPIConfig Init Failed!!!");
             logger.error(e.getMessage());
         }
+    }
+
+
+    @PostConstruct
+    @Async
+    @Scheduled(fixedDelay = 60000) // 60秒更新一次
+    public void updateLocalSettings(){
+        String tempSQL = "SELECT * FROM channel_bind";
+        channelBinds = jdbcTemplate.query(tempSQL, new BeanPropertyRowMapper<>(ChannelBind.class));
     }
 }
